9044c79966cb6c98c7263743f2cac058fe218125
Magnus Fredheim Hanssen <mha430@post.uit.no>
2022-10-08 22:51:20 +0200

    Finished "game"

--- Main.gd
+++ Main.gd
@@ -1,19 +1,58 @@
 extends Node
 
+export (PackedScene) var flag_scene
+export (PackedScene) var hull_scene
+
+var collide = true
+
+var current_vec = Vector3.ZERO
+var last_vec = current_vec
+
 var time = 0.0
+var point_time = 0.0
 
 var parser = SpheroParser.new()
 
-var count = 0
 
-func _ready():
+func getPath(name: String) -> String:
 	var path = ""
 	if OS.has_feature("editor"):
-		path = ProjectSettings.globalize_path("res://sensor_data/Accelerometer.csv")
+		path = ProjectSettings.globalize_path("res://" + name)
 	else:
-		path = OS.get_executable_path().get_base_dir().plus_file("sensor_data/Accelerometer.csv")
-	parser.open_file(path)
+		path = OS.get_executable_path().get_base_dir().plus_file(name)
+	return path
+	
+func _ready():
+	var name = $SpheroBolt.directory
+	parser.open_file(getPath(name + "/Velocity.csv"))
 
-func _process(_delta):
-	print("Read line: %s" % parser.read_line())
-	count += 1
+func _process(delta):
+	while (time + delta > point_time) and parser.is_open():
+		parser.read_line()
+		
+		if collide:
+			var flag = flag_scene.instance()
+			flag.translate($SpheroBolt.point_pos)
+			add_child(flag)
+			collide = false
+		
+		last_vec = current_vec
+		current_vec = parser.yield_vector()
+		point_time = parser.yield_time()
+		
+		collide = current_vec.z - last_vec.z < -60.0
+
+	time += delta
+
+
+func _on_SpheroBolt_hull_formed():
+	$DrawHull.begin(Mesh.PRIMITIVE_LINE_STRIP)
+	$DrawHull.set_color(Color.darkgreen)
+	for vec in $SpheroBolt.convex_hull:
+		var flag = hull_scene.instance()
+		flag.translate(vec)
+		add_child(flag)
+		$DrawHull.add_vertex(Vector3(vec.x, vec.y + 1.0, vec.z))
+	$DrawHull.end()
+	print("Hull drawn")
+	print($SpheroBolt.convex_hull.size())
--- SpheroBolt.gd
+++ SpheroBolt.gd
@@ -1,29 +1,61 @@
 extends KinematicBody
 
+signal hull_formed
 
-# Declare member variables here. Examples:
-export var max_speed  = 50
+export var directory = "many_walls"
 
 var velocity = Vector3.ZERO
 var p_velocity = velocity
 
+var loc_parser = SpheroParser.new()
+
+var curr_time = 0.0
+var point_time = 0.0
+export var point_pos = Vector3.ZERO
+
+var loop = true
+
+var scanner = GrahamScanner.new()
+var show_convex = true
+
+export var convex_hull = []
+
+func getPath(name: String) -> String:
+	var path = ""
+	if OS.has_feature("editor"):
+		path = ProjectSettings.globalize_path("res://" + name)
+	else:
+		path = OS.get_executable_path().get_base_dir().plus_file(name)
+	return path
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Add code to bind input file somehow
+	loc_parser.open_file(getPath(directory + "/Location.csv"))
 	
 func _physics_process(delta):
-	var direction = Vector3.ZERO
-	
-	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	direction.z = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	
-	p_velocity = velocity
-	
-	velocity.x = direction.x * max_speed
-	velocity.z = direction.z * max_speed
-	
-	$Pivot.rotate_x(2 * velocity.z/360)
-	$Pivot.rotate_z(- 2 * velocity.x/360)
-	
-	velocity = move_and_slide(velocity, Vector3.UP)
-	
+	loop = loc_parser.is_open()
+	while (curr_time + delta > point_time) and loop:
+		loc_parser.read_line()
+		point_time = loc_parser.yield_time()
+		point_pos = loc_parser.yield_vector()/5
+		point_pos.z = point_pos.y
+		point_pos.y = 0;
+		scanner.insert_point(point_pos.x, point_pos.z)
+		loop = loc_parser.is_open()
+		
+	if loop:
+		velocity = (point_pos - global_transform.origin)/(point_time - curr_time)
+		
+		velocity.y = 0.0
+		curr_time += delta
+		
+		$Pivot.rotate_x(2 * velocity.z/360)
+		$Pivot.rotate_z(- 2 * velocity.x/360)
+		
+		
+		velocity = move_and_slide(velocity, Vector3.UP)
+	else:
+		if show_convex:
+			convex_hull = scanner.calculate_hull()
+			emit_signal("hull_formed")
+			show_convex = false
