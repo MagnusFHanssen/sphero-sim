aeea181c2695fa7ceb0ca68da47e62ef69894ebe
Magnus Fredheim Hanssen <mha430@post.uit.no>
2022-10-10 14:37:02 +0200

    Final change, added WASD controls and directional light

--- SpheroCamera.gd
+++ SpheroCamera.gd
@@ -1,4 +1,16 @@
 extends Camera
 
+
 func _process(_delta):
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.z += 1
+	if Input.is_action_pressed("move_up"):
+		direction.z -= 1
+	
+	global_transform.origin += direction
 	look_at($"../SpheroBolt".global_transform.origin, Vector3.UP)
